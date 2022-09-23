94d8585326353c8d605a3c41fe5b56903cecc3af
dimateniuta <dteniuta@gmail.com>
2022-09-23 18:49:38 +0300

    feat: add About component

---
+++ react.components/src/components/About/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './About.css';
+
+export default function About() {
+  return (
+    <div className="about__wrap">
+      <h1 className="about__title">Rolling Scopes School</h1>
+      <p className="about__content">React Course</p>
+    </div>
+  );
+}
