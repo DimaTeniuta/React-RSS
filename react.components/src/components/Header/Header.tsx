66943d83f4071d12e5c29d05c136bc7dfcb77dde
dimateniuta <dteniuta@gmail.com>
2022-09-25 18:52:52 +0300

    refactor: change Header component styles extension to .scss

--- react.components/src/components/Header/Header.tsx
+++ react.components/src/components/Header/Header.tsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css';
+import classes from './Header.module.scss';
 
 export default class Header extends Component {
   render() {
     return (
-      <header className="header">
-        <div className="header__wrap-btn">
-          <Link className="header__nav-link" to="/" data-testid="about-link">
+      <header className={classes.header}>
+        <div className={classes.wrapBtn}>
+          <Link className={classes.navLink} to="/" data-testid="about-link">
             About Us
           </Link>
-          <Link className="header__nav-link" to="/main" data-testid="main-link">
+          <Link className={classes.navLink} to="/main" data-testid="main-link">
             Main
           </Link>
         </div>
