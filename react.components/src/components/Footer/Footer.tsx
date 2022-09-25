cfab46e43f25e06fa2f9a3cf841576dd521a31c5
dimateniuta <dteniuta@gmail.com>
2022-09-25 18:43:26 +0300

    refactor: change Footer component styles extension to .scss

--- react.components/src/components/Footer/Footer.tsx
+++ react.components/src/components/Footer/Footer.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import './Footer.css';
+import classes from './Footer.module.scss';
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer__container">
-        <div className="as">
+    <footer className={classes.footer}>
+      <div className={classes.container}>
+        <div className={classes.gitHub}>
           <a
-            className="footer__gitHubLink"
+            className={classes.gitHubLink}
             target="_blank"
             href="https://github.com/DimaTeniuta"
             rel="noreferrer"
           ></a>
         </div>
-        <div className="footer__year">2022</div>
+        <div className={classes.year}>2022</div>
         <div>
           <a
-            className="footer__rssLink"
+            className={classes.rssLink}
             target="_blank"
             href="https://rs.school/js/"
             rel="noreferrer"
