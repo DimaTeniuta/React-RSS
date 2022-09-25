a0f4ce0eafefa218d7e40058397531fd53e582ea
dimateniuta <dteniuta@gmail.com>
2022-09-25 18:36:02 +0300

    refactor: change Error404 component styles extension to .scss

--- react.components/src/components/Error404/Error404.tsx
+++ react.components/src/components/Error404/Error404.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactNode } from 'react';
-import './Error404.css';
+import classes from './Error404.module.scss';
 
 interface Error404Props {
   children: ReactNode;
@@ -8,9 +8,9 @@ interface Error404Props {
 
 const Error404: FC<Error404Props> = ({ children, ...props }) => {
   return (
-    <div className="error__wrap" data-testid="error-page">
-      <h1 className="error__title">{props.title}</h1>
-      <p className="error__message">{children}</p>
+    <div className={classes.wrap} data-testid="error-page">
+      <h1 className={classes.title}>{props.title}</h1>
+      <p className={classes.message}>{children}</p>
     </div>
   );
 };
