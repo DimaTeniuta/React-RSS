22a4aa1dd66dfa479740312219b92d709f4b1a49
dimateniuta <dteniuta@gmail.com>
2022-10-01 13:29:07 +0300

    refactor: correct comments after code review in Card.tsx file

--- react.components/src/components/Card/Card.tsx
+++ react.components/src/components/Card/Card.tsx
@@ -2,22 +2,20 @@ import Button from 'components/UI/Button/Button';
 import React from 'react';
 import { ResultsData } from 'types/generalTypes';
 import classes from './Card.module.scss';
-import btnClasses from '../UI/Button/Button.module.scss';
 
 type DataCard = {
   data: ResultsData;
 };
 
-export const Card: React.FC<DataCard> = (props) => {
-  const data = props.data;
+export const Card: React.FC<DataCard> = ({ data }) => {
   return (
     <div className={classes.card} data-testid="test-card">
       <img className={classes.images} src={data.urls?.small} alt="card-image" />
-      <p className={classes.title}>{data.description ? data.description : 'Unknown'}</p>
+      <p className={classes.title}>{data.description ?? 'Unknown'}</p>
       <span className={classes.likes}>{data.likes}</span>
       <div className={classes.icon}></div>
       <form action={data.urls?.full} target="_blank">
-        <Button className={btnClasses.standardBtn}>Full size</Button>
+        <Button className={classes.standardBtn}>Full size</Button>
       </form>
     </div>
   );
