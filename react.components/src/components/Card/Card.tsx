4adcca0a21a556a8b70a130f5774694b9c3f2e1e
dimateniuta <dteniuta@gmail.com>
2022-10-18 09:52:27 +0300

    feat: rewrote Card component

--- react.components/src/components/Card/Card.tsx
+++ react.components/src/components/Card/Card.tsx
@@ -1,44 +1,32 @@
 import Button from 'components/UI/Button/Button';
-import React, { Component } from 'react';
+import React, { FC, useState } from 'react';
 import { ResultsData } from 'types/generalTypes';
 import classes from './Card.module.scss';
 import { CardModalWindow } from 'components/CardModalWindow/CardModalWindow';
-
-interface StateCard {
-  isActiveModalWindow: boolean;
-}
 
 type PropsCard = {
   data: ResultsData;
 };
 
-export default class Card extends Component<PropsCard, StateCard> {
-  constructor(props: PropsCard) {
-    super(props);
-    this.state = {
-      isActiveModalWindow: false,
-    };
-  }
-
-  toggleModalWindow = (): void => {
-    this.setState({ isActiveModalWindow: !this.state.isActiveModalWindow });
+const Card: FC<PropsCard> = ({ data }): JSX.Element => {
+  const [isActiveModalWindow, setIsActiveModalWindow] = useState<boolean>(false);
+  const toggleModalWindow = (): void => {
+    setIsActiveModalWindow(!isActiveModalWindow);
   };
 
-  render() {
-    return (
-      <>
-        <div className={classes.card} data-testid="test-card">
-          <img className={classes.images} src={this.props.data.urls?.small} alt="card-image" />
-          <p className={classes.title}>{this.props.data.description ?? 'Unknown'}</p>
-          <span className={classes.likes}>{this.props.data.likes}</span>
-          <div className={classes.icon}></div>
-          <Button onClick={this.toggleModalWindow}>More details</Button>
-        </div>
+  return (
+    <>
+      <div className={classes.card} data-testid="test-card">
+        <img className={classes.images} src={data.urls?.small} alt="card-image" />
+        <p className={classes.title}>{data.description ?? 'Unknown'}</p>
+        <span className={classes.likes}>{data.likes}</span>
+        <div className={classes.icon}></div>
+        <Button onClick={toggleModalWindow}>More details</Button>
+      </div>
 
-        {this.state.isActiveModalWindow && (
-          <CardModalWindow onClick={this.toggleModalWindow} data={this.props.data} />
-        )}
-      </>
-    );
-  }
-}
+      {isActiveModalWindow && <CardModalWindow onClick={toggleModalWindow} data={data} />}
+    </>
+  );
+};
+
+export default Card;
