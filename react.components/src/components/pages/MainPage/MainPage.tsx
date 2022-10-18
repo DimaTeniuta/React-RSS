e6080f673430eab2f5aeda3dff7fa19dcca3fb3b
dimateniuta <dteniuta@gmail.com>
2022-10-18 10:11:32 +0300

    feat: rewrote MainPage component into functional component

--- react.components/src/components/pages/MainPage/MainPage.tsx
+++ react.components/src/components/pages/MainPage/MainPage.tsx
@@ -1,50 +1,37 @@
 import TextWindow from 'components/TextWindow/TextWindow';
 import Search from 'components/Search/Search';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { ResultsData } from 'types/generalTypes';
 import classes from './MainPage.module.scss';
 import Card from 'components/Card/Card';
 import { Loader } from 'components/Loader/Loader';
 
-type StateMain = {
-  data: ResultsData[];
-  isLoading: boolean;
+const MainPage = (): JSX.Element => {
+  const [data, setData] = useState<ResultsData[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const getCards = (data: ResultsData[]): void => {
+    setData(data);
+    setIsLoading(false);
+  };
+  const turnOnLoader = (): void => {
+    setIsLoading(true);
+  };
+
+  return (
+    <div className={classes.container} data-testid="main-page">
+      <Search getData={getCards} onLoader={turnOnLoader} />
+
+      <div className={classes.wrapCards}>
+        {isLoading ? (
+          <Loader />
+        ) : data.length ? (
+          data.map((el) => <Card key={el.id} data={el} />)
+        ) : (
+          <TextWindow title="Not Found">Try entering another query</TextWindow>
+        )}
+      </div>
+    </div>
+  );
 };
 
-type PropsMain = object;
-
-export default class MainPage extends Component<PropsMain, StateMain> {
-  constructor(props: PropsMain) {
-    super(props);
-    this.state = {
-      data: [],
-      isLoading: false,
-    };
-  }
-
-  getCards = (data: ResultsData[]): void => {
-    this.setState({ data, isLoading: false });
-  };
-
-  turnOnLoader = (): void => {
-    this.setState({ isLoading: true });
-  };
-
-  render() {
-    return (
-      <div className={classes.container} data-testid="main-page">
-        <Search getData={this.getCards} onLoader={this.turnOnLoader} />
-
-        <div className={classes.wrapCards}>
-          {this.state.isLoading ? (
-            <Loader />
-          ) : this.state.data.length ? (
-            this.state.data.map((el) => <Card key={el.id} data={el} />)
-          ) : (
-            <TextWindow title="Not Found">Try entering another query</TextWindow>
-          )}
-        </div>
-      </div>
-    );
-  }
-}
+export default MainPage;
