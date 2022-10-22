4e3c23b2d0f2f80a6ec9a4ba2708bb7f9d64914c
dimateniuta <dteniuta@gmail.com>
2022-10-22 10:48:44 +0300

    feat: add input checkbox in Forms component

--- react.components/src/components/Forms/Forms.tsx
+++ react.components/src/components/Forms/Forms.tsx
@@ -1,5 +1,5 @@
 import Input from 'components/UI/Input/Input';
-import React, { FC, useEffect, useRef, useState } from 'react';
+import React, { FC } from 'react';
 import classes from './Forms.module.scss';
 import SELECTOR_OPTIONS from '../../data/optionsForSelect.json';
 import Button from 'components/UI/Button/Button';
@@ -7,28 +7,10 @@ import Select from 'components/UI/Select/Select';
 import InputFile from 'components/UI/InputFile/InputFile';
 import { InputSwitch } from 'components/UI/InputSwitch/InputSwitch';
 import InputCheckbox from 'components/UI/InputCheckbox/InputCheckbox';
-import { FormData, SelectData } from 'types/formTypes';
-import {
-  validateDateInput,
-  validateInputCheckbox,
-  validateInputFile,
-  validateSelectInput,
-  validateTextInput,
-} from 'utils/validator';
-import { Controller, SubmitHandler, useForm, UseFormReturn } from 'react-hook-form';
-import { urlToHttpOptions } from 'url';
-import { getValue } from '@testing-library/user-event/dist/utils';
+import { FormData } from 'types/formTypes';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 
 const DEFAULT_VALUE_COUNTRY = 'Country';
-
-enum ErrorFieldNames {
-  NAME = 'nameError',
-  SURNAME = 'surnameError',
-  BIRTHDAY = 'birthdayError',
-  COUNTRY = 'countryError',
-  PERSONAL_DATA = 'personaDataError',
-  AVATAR = 'avatarError',
-}
 
 enum ErrorsForm {
   REQUIRED_FIELD = 'This field is required',
@@ -50,6 +32,7 @@ export interface FormInputs {
   country: string;
   avatar: FileList;
   gender: boolean;
+  personalData: boolean;
 }
 
 export const Forms: FC<PropsForms> = ({ addData }): JSX.Element => {
@@ -68,8 +51,8 @@ export const Forms: FC<PropsForms> = ({ addData }): JSX.Element => {
       birthday: data.birthday,
       country: data.country,
       avatar: URL.createObjectURL(data.avatar[0]),
-      personalData: true,
-      genderMale: false,
+      personalData: data.personalData,
+      genderMale: data.gender,
     };
     addData(obj);
   };
@@ -169,214 +152,18 @@ export const Forms: FC<PropsForms> = ({ addData }): JSX.Element => {
 
       <InputSwitch label="switch" title="Male/Female:" {...register('gender')} />
 
+      <InputCheckbox
+        label="agree"
+        title="Consent to data processing:"
+        {...register('personalData', {
+          required: ErrorsForm.REQUIRED_FIELD,
+        })}
+        error={errors?.personalData?.message}
+      />
+
       <Button>Post</Button>
     </form>
   );
 };
-
-// interface Errors {
-//   nameError: string;
-//   surnameError: string;
-//   birthdayError: string;
-//   countryError: string;
-//   avatarError: string;
-//   personaDataError: string;
-// }
-
-// const Forms: FC<PropsForms> = ({ addData }): JSX.Element => {
-//   const [isFirstInput, setIsFirstInput] = useState<boolean>(false);
-//   const [isDisabled, setIsDisabled] = useState<boolean>(true);
-//   const [isValid, setIsValid] = useState<boolean>(false);
-//   const [isDone, setIsDone] = useState<boolean>(false);
-//   const [isValidAvatar, setIsValidAvatar] = useState<boolean>(false);
-//   const [errors, setErrors] = useState<Errors>({
-//     nameError: '',
-//     surnameError: '',
-//     birthdayError: '',
-//     countryError: '',
-//     avatarError: '',
-//     personaDataError: '',
-//   });
-//   const nameRef = useRef<HTMLInputElement>(null);
-//   const surnameRef = useRef<HTMLInputElement>(null);
-//   const birthdayRef = useRef<HTMLInputElement>(null);
-//   const countryRef = useRef<HTMLSelectElement>(null);
-//   const avatarRef = useRef<HTMLInputElement>(null);
-//   const personalDataRef = useRef<HTMLInputElement>(null);
-//   const switchRef = useRef<HTMLInputElement>(null);
-
-//   const firstInput = (): void => {
-//     if (
-//       nameRef.current?.value ||
-//       surnameRef.current?.value ||
-//       birthdayRef.current?.value ||
-//       countryRef.current?.value !== DEFAULT_VALUE_COUNTRY ||
-//       avatarRef.current?.value ||
-//       switchRef.current?.checked ||
-//       personalDataRef.current?.checked
-//     ) {
-//       setIsDisabled(false);
-//       setIsFirstInput(true);
-//       setIsDone(false);
-//     }
-//   };
-
-//   const onClickAvatar = (): void => {
-//     avatarRef.current?.click();
-//   };
-
-//   const onClickSwitch = (): void => {
-//     switchRef.current?.click();
-//   };
-
-//   const onClickPersonalData = (): void => {
-//     personalDataRef.current!.click();
-//   };
-
-//   const validateForm = (): boolean => {
-//     const errors: Errors = {
-//       [ErrorFieldNames.NAME]: validateTextInput(nameRef.current?.value),
-//       [ErrorFieldNames.SURNAME]: validateTextInput(surnameRef.current?.value),
-//       [ErrorFieldNames.BIRTHDAY]: validateDateInput(birthdayRef.current?.value),
-//       [ErrorFieldNames.COUNTRY]: validateSelectInput(
-//         countryRef.current?.value,
-//         DEFAULT_VALUE_COUNTRY
-//       ),
-//       [ErrorFieldNames.PERSONAL_DATA]: validateInputCheckbox(personalDataRef.current?.checked),
-//       [ErrorFieldNames.AVATAR]: validateInputFile(avatarRef.current?.files),
-//     };
-//     setErrors(errors);
-//     errors[ErrorFieldNames.AVATAR] ? setIsValidAvatar(false) : setIsValidAvatar(true);
-
-//     return Object.entries(errors).every((item) => !item[1]);
-//   };
-
-//   const validateAfterWrongPost = (): void => {
-//     const isValidForm = validateForm();
-//     if (isValidForm) {
-//       setIsDisabled(false);
-//     }
-//   };
-
-//   const handleOnChange = () => {
-//     if (!isFirstInput) {
-//       firstInput();
-//     }
-
-//     if (isValid) {
-//       validateAfterWrongPost();
-//     }
-//   };
-
-//   const clearForm = (event: React.FormEvent<HTMLFormElement>): void => {
-//     setIsFirstInput(false);
-//     setIsDisabled(true);
-//     setIsValid(false);
-//     setIsValidAvatar(false);
-//     const form = event?.target as HTMLFormElement;
-//     form.reset();
-//     switchRef.current!.checked = false;
-//     personalDataRef.current!.checked = false;
-//   };
-
-//   const postData = () => {
-//     const cardData: FormData = {
-//       name: nameRef.current!.value,
-//       surname: surnameRef.current!.value,
-//       birthday: birthdayRef.current!.value,
-//       country: countryRef.current!.value,
-//       avatar: avatarRef.current!.files![0],
-//       personalData: personalDataRef.current!.checked,
-//       genderMale: switchRef.current!.checked,
-//     };
-//     addData(cardData);
-//   };
-
-//   const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-//     event.preventDefault();
-//     setIsDisabled(true);
-//     setIsValid(true);
-//     const isValidForm = validateForm();
-//     if (isValidForm) {
-//       postData();
-//       setIsDone(true);
-//       clearForm(event);
-//     }
-//   };
-
-//   return (
-//     <form
-//       className={classes.container}
-//       onSubmit={handleSubmit}
-//       onChange={handleOnChange}
-//       noValidate
-//       data-testid="forms"
-//     >
-//       <Input
-//         label="firstName"
-//         title="Name:"
-//         type="text"
-//         ref={nameRef}
-//         testid="inputName"
-//         error={errors.nameError}
-//       />
-
-//       <Input
-//         label="surname"
-//         title="Surname:"
-//         type="text"
-//         ref={surnameRef}
-//         testid="inputSurname"
-//         error={errors.surnameError}
-//       />
-
-//       <Input
-//         label="birthday"
-//         title="Birthday:"
-//         type="date"
-//         ref={birthdayRef}
-//         testid="inputDate"
-//         error={errors.birthdayError}
-//       />
-
-//       <Select
-//         label="country"
-//         title="Country:"
-//         defaultValue={DEFAULT_VALUE_COUNTRY}
-//         options={SELECTOR_OPTIONS}
-//         ref={countryRef}
-//         error={errors.countryError}
-//       />
-
-//       <InputFile
-//         label="avatar"
-//         title="Avatar:"
-//         ready={isValidAvatar.toString()}
-//         ref={avatarRef}
-//         onClick={onClickAvatar}
-//         error={errors.avatarError}
-//       />
-
-//       <InputSwitch label="switch" title="Male/Female:" onClick={onClickSwitch} ref={switchRef} />
-
-//       <InputCheckbox
-//         label="agree"
-//         title="Consent to data processing:"
-//         onClick={onClickPersonalData}
-//         ref={personalDataRef}
-//         error={errors.personaDataError}
-//       />
-
-//       <Button disabled={isDisabled}>Post</Button>
-
-//       {isDone && (
-//         <div className={classes.done} data-testid="final-text">
-//           Done
-//           <span className={classes.doneImg} data-testid="final-img"></span>
-//         </div>
-//       )}
-//     </form>
-//   );
-// };
 
 export default Forms;
--- react.components/src/components/UI/InputCheckbox/InputCheckbox.tsx
+++ react.components/src/components/UI/InputCheckbox/InputCheckbox.tsx
@@ -6,26 +6,28 @@ interface InputCheckboxProps {
   label: string;
   title: string;
   ref?: React.ForwardedRef<HTMLInputElement>;
-  onClick?: () => void;
-  onChange?: (event?: React.ChangeEvent<HTMLInputElement>) => void;
   error?: string;
 }
 
 const InputCheckbox: FC<InputCheckboxProps> = React.forwardRef((props, ref): JSX.Element => {
   return (
-    <Label label={props.label} title={props.title} className={classes.wrap}>
-      <div className={classes.box} onClick={props.onClick} data-testid="btn-inputCheckbox">
-        <input
-          {...props}
-          type="checkbox"
-          className={classes.input}
-          ref={ref}
-          data-testid="inputCheckbox"
-        />
-        <span className={classes.img}></span>
-      </div>
-      <p className={classes.error}>{props.error}</p>
-    </Label>
+    <>
+      <div className={classes.title}>{props.title}</div>
+      <Label label={props.label} className={classes.wrap}>
+        <div className={classes.box} data-testid="btn-inputCheckbox">
+          <input
+            {...props}
+            id={props.label}
+            type="checkbox"
+            className={classes.input}
+            ref={ref}
+            data-testid="inputCheckbox"
+          />
+          <span className={classes.img}></span>
+        </div>
+        <p className={classes.error}>{props.error}</p>
+      </Label>
+    </>
   );
 });
 
